46cac2f15de6ecf2ce8d376647d202dc27db712f
Agata Kamińska <a.fluffypie@gmail.com>
2024-06-01 20:33:08 +0200

    event listener

--- script.js
+++ script.js
@@ -8,9 +8,25 @@ Dodaj do elementu <img> atrybut data-test=”img-goat”.
 Chociaż nie musisz wykorzystywać do tego własnoręcznie napisanego requestu, to czy wiesz, że “pod maską” stworzony tag wykonuje zapytanie GET? */
 
 const createButton = () => {
-    const button = document.createElement("button");
-    button.id = "getGoat";
-    button.innerText = "Pobierz obraz";
-    document.body.appendChild(button);
-    return button
-}
+  const button = document.createElement("button");
+  button.id = "getGoat";
+  button.innerText = "Wyświetl obraz";
+  document.body.appendChild(button);
+  return button;
+};
+const clickedButton = createButton();
+clickedButton.addEventListener("click", () => {
+  const url =
+    "https://picsum.photos/300/200?random&t=" + new Date().getTime();
+  const getImage = document.createElement("img");
+  getImage.setAttribute("data-test", "img-goat");
+  getImage.src = url;
+  getImage.onload = () => {
+    document.body.appendChild(getImage);
+  };
+  getImage.onerror = () => {
+    const error = document.createElement("p");
+    error.innerText = "Nie można wyświetlić obrazu. Spróbuj ponownie później.";
+    document.body.appendChild(error);
+  };
+});
