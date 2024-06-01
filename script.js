a1c7774da63c68a32652d05f39af319e5c07eb69
Agata Kamińska <a.fluffypie@gmail.com>
2024-06-01 20:33:30 +0200

    deleted comment

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-/* Przygotuj ponownie przycisk z atrybutem id=”getGoat”. Spraw aby po kliknięciu w niego dodany został dynamicznie element <img> o src podanym niżej:
-https://picsum.photos/
-
-Nie wystarczy jednak sam adres powyżej - musisz jeszcze dodać do ścieżki informacje o rozmiarze w następującym formacie: https://picsum.photos/szerokosc/wysokosc gdzie szerokość powinna być równa 300px, a wysokość 200px. 
-
-Dodaj do elementu <img> atrybut data-test=”img-goat”.
-
-Chociaż nie musisz wykorzystywać do tego własnoręcznie napisanego requestu, to czy wiesz, że “pod maską” stworzony tag wykonuje zapytanie GET? */
-
 const createButton = () => {
   const button = document.createElement("button");
   button.id = "getGoat";
@@ -16,8 +7,7 @@ const createButton = () => {
 };
 const clickedButton = createButton();
 clickedButton.addEventListener("click", () => {
-  const url =
-    "https://picsum.photos/300/200?random&t=" + new Date().getTime();
+  const url = "https://picsum.photos/300/200?random&t=" + new Date().getTime();
   const getImage = document.createElement("img");
   getImage.setAttribute("data-test", "img-goat");
   getImage.src = url;
